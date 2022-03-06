75cef3fd5c7e152768a711fa324ff246ecf3f91b
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 11:26:35 +0300

    Will have forms for editing a tutorial by id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTutorialComponent
+    AddTutorialComponent,
+    TutorialDetailsComponent
   ],
   imports: [
     BrowserModule,
