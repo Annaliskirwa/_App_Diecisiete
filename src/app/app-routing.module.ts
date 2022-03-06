7404eaec9b4b82044c89c6d7760b35ffd14121ca
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 12:42:00 +0300

    Created path for tutorial lists

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'tutorials', pathMatch: 'full'},
+  {path: 'tutorials', component: TutorialsListComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
