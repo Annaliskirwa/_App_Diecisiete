0723b70436c9d43d6725ec45559d765b30ae98a4
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 16:49:13 +0300

    Resetting form to defaults for another tutorial

--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -25,7 +25,20 @@ export class AddTutorialComponent implements OnInit {
       title: this.tutorial.title,
       description: this.tutorial.description
     };
+    this.tutorialService.create(data).subscribe(response=>{
+      console.log(response);
+      this.submitted = true;
+    },
+    error =>{
+      console.log(error);
+    });
   }
-
-  
+  newTutorial():void{
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published: false
+    };
+  }
 }
