131fe3527f8a8844a313d72ba587537884e04d88
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 11:25:33 +0300

    Will contain forms for adding a new tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTutorialComponent
   ],
   imports: [
     BrowserModule,
