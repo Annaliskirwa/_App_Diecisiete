8b962c190e4d14b8988e587999591a7317ab287c
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 13:09:19 +0300

    Added the base url

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
+const baseUrl = 'http://127.0.0.1:8000/api/tutorials';
+
 @Injectable({
   providedIn: 'root'
 })
