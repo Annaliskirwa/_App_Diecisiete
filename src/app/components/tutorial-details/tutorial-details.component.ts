1794e2fbcaa19ea2fbb5baaac3b6bbb15c517e0f
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 22:32:47 +0300

    Getting a single tutorial by id

--- src/app/components/tutorial-details/tutorial-details.component.ts
+++ src/app/components/tutorial-details/tutorial-details.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TutorialService } from 'src/app/services/tutorial.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tutorial } from 'src/app/models/tutorial.model';
+
 @Component({
   selector: 'app-tutorial-details',
   templateUrl: './tutorial-details.component.html',
   styleUrls: ['./tutorial-details.component.css']
 })
 export class TutorialDetailsComponent implements OnInit {
+  currentTutorial: Tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  message = '';
 
-  constructor() { }
+  constructor(
+    private tutorialService: TutorialService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getTutorial(this.route.snapshot.params.id);
   }
-
+  getTutorial(id:string):void{
+    this.tutorialService.get(id).subscribe(data => {this.currentTutorial = data;
+    console.log(data);
+  },
+  error =>{
+    console.log(error);
+  });
+  }
+  
 }
