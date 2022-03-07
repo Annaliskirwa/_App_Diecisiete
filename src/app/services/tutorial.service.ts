9769ccd9da00eeaa4ea83c0dc3dbb006d0ae463a
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-07 05:24:24 +0300

    Changed the base url to the hosted url

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://127.0.0.1:8000/api/tutorials';
+const baseUrl = 'https://anntutorial.herokuapp.com/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
