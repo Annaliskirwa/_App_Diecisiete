6c1e1141cea5b894cdad8b52d84625b8816f8749
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-07 06:16:08 +0300

    updates to deploy to herokuu

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/tutorials'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/tutorials/index.html'));});
 app.listen(process.env.PORT || 8080);
